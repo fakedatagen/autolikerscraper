5bfb45231e64ab2df81f51b71528ea8d71fe68d9
fakedatagen <130356758+fakedatagen@users.noreply.github.com>
2025-10-29 15:15:37 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -12,7 +12,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 from scraper import run_scraper
 
 DB_PATH = "botdata.db"
-TELEGRAM_TOKEN = os.getenv("8379824269:AAFNf9RsAHc2E2gIGHokPSsF-oDFwiJK0lo")
+TELEGRAM_TOKEN = "8379824269:AAFNf9RsAHc2E2gIGHokPSsF-oDFwiJK0lo"
 
 if not TELEGRAM_TOKEN:
     print("❌ TELEGRAM_BOT_TOKEN missing in secrets!")
